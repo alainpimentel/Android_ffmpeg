814200fc4386569414d64e740ecd9215c5c4e704
Alain Pimentel <hpimentel@southern.edu>
2014-05-23 09:34:41 -0700

    Only show video files in browser. Return path to selected file

--- src/com/example/ffmpeg_trial/FragmentBrowser.java
+++ src/com/example/ffmpeg_trial/FragmentBrowser.java
@@ -1,23 +1,24 @@
 package com.example.ffmpeg_trial;
 
+import java.io.File;
+
 import android.app.Fragment;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.ipaulpro.afilechooser.utils.FileUtils;
 
+public class FragmentBrowser extends Fragment implements OnClickListener {
 
-public class FragmentBrowser extends Fragment {
-
-	ImageView ivIcon;
-	TextView tvItemName;
-
-	public static final String IMAGE_RESOURCE_ID = "iconResourceID";
-	public static final String ITEM_NAME = "itemName";
+	private static final int REQUEST_CHOOSER = 1234;
+	Button browse_button;
 
 	public FragmentBrowser() {
 
@@ -26,10 +27,43 @@ public class FragmentBrowser extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_layout_browser,container,false);
-		
-		Intent i = new Intent(getActivity(), ActivityaFileChooserLink.class);
-		startActivity(i);
-	    return view;
+
+		View view = inflater.inflate(R.layout.fragment_layout_browser, container, false);
+
+		browse_button = (Button) view.findViewById(R.id.button1);
+		browse_button.setOnClickListener(this);
+		return view;
 	}
+
+	@Override
+	public void onClick(View v) {
+		Toast.makeText(getActivity(), "Clicked on button", Toast.LENGTH_LONG).show();
+		// Create the ACTION_GET_CONTENT Intent
+		Intent getContentIntent = FileUtils.createGetContentIntent();
+
+		Intent intent = Intent.createChooser(getContentIntent, "Select a file");
+		startActivityForResult(intent, REQUEST_CHOOSER);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    switch (requestCode) {
+	        case REQUEST_CHOOSER:   
+	            if (resultCode == getActivity().RESULT_OK) {
+
+	                final Uri uri = data.getData();
+	                
+	                // Get the File path from the Uri
+	                String path = FileUtils.getPath(getActivity(), uri);
+	                Toast.makeText(getActivity(), path, Toast.LENGTH_LONG).show();
+
+	                // Alternatively, use FileUtils.getFile(Context, Uri)
+	                if (path != null && FileUtils.isLocal(path)) {
+	                    File file = new File(path);
+	                }
+	            }
+	            break;
+	    }
+	}
+
 }
--- src/com/example/ffmpeg_trial/MainActivity.java
+++ src/com/example/ffmpeg_trial/MainActivity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -17,234 +18,214 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
-	
+
 	private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
 
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    CustomDrawerAdapter adapter;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+	CustomDrawerAdapter adapter;
 
-    List<DrawerItem> dataList;
+	List<DrawerItem> dataList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        /*
-        videoHandler vh = new videoHandler();
-        ArrayList<Clip> in = new ArrayList<Clip>();
-        final Clip clip_in = new Clip("/storage/emulated/0/Developer/test.mp4"); // input source
-        clip_in.isVideo(); 
-        in.add(clip_in);
-        boolean video = new File( "/storage/emulated/0/Developer/result.mp4").exists();
-        Toast.makeText(MainActivity.this, "result: " + video, Toast.LENGTH_LONG).show();
-        
-        Activity activity = (Activity) MainActivity.this;
-		File fileTmp = activity.getCacheDir(); 
-		File fileAppRoot = new File(activity.getApplicationInfo().dataDir);
-		//String dir =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getAbsolutePath();
-		//Toast.makeText(MainActivity.this, "result: " + dir, Toast.LENGTH_LONG).show();
-		final Clip clip_out = new Clip("/storage/emulated/0/Developer/result2.mp4");
-		//put flags in clip
-		clip_out.videoFps = "30";
-		clip_out.width = 480;
-		clip_out.height = 320;
-		clip_out.videoCodec = "libx264";
-		clip_out.audioCodec = "copy";
-		
-	    try {
-			FfmpegController fc = new FfmpegController(fileTmp, fileAppRoot);
-			// works! //fc.convertToMPEG(clip_in, "/storage/emulated/0/Developer/result.mp4", 
-			fc.processVideo(clip_in, clip_out, false, new ShellUtils.ShellCallback() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/*
+		 * videoHandler vh = new videoHandler(); ArrayList<Clip> in = new
+		 * ArrayList<Clip>(); final Clip clip_in = new
+		 * Clip("/storage/emulated/0/Developer/test.mp4"); // input source
+		 * clip_in.isVideo(); in.add(clip_in); boolean video = new File(
+		 * "/storage/emulated/0/Developer/result.mp4").exists();
+		 * Toast.makeText(MainActivity.this, "result: " + video,
+		 * Toast.LENGTH_LONG).show();
+		 * 
+		 * Activity activity = (Activity) MainActivity.this; File fileTmp =
+		 * activity.getCacheDir(); File fileAppRoot = new
+		 * File(activity.getApplicationInfo().dataDir); //String dir =
+		 * Environment
+		 * .getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES
+		 * ).getAbsolutePath(); //Toast.makeText(MainActivity.this, "result: " +
+		 * dir, Toast.LENGTH_LONG).show(); final Clip clip_out = new
+		 * Clip("/storage/emulated/0/Developer/result2.mp4"); //put flags in
+		 * clip clip_out.videoFps = "30"; clip_out.width = 480; clip_out.height
+		 * = 320; clip_out.videoCodec = "libx264"; clip_out.audioCodec = "copy";
+		 * 
+		 * try { FfmpegController fc = new FfmpegController(fileTmp,
+		 * fileAppRoot); // works! //fc.convertToMPEG(clip_in,
+		 * "/storage/emulated/0/Developer/result.mp4", fc.processVideo(clip_in,
+		 * clip_out, false, new ShellUtils.ShellCallback() {
+		 * 
+		 * @Override public void shellOut(String shellLine) {
+		 * System.out.println("MIX> " + shellLine); }
+		 * 
+		 * @Override public void processComplete(int exitValue) {
+		 * 
+		 * if (exitValue != 0) { System.err.println("concat non-zero exit: " +
+		 * exitValue); Log.d("ffmpeg","Compilation error. FFmpeg failed");
+		 * Toast.makeText(MainActivity.this, "result: ffmpeg failed",
+		 * Toast.LENGTH_LONG).show(); } else { if(new File(
+		 * "/storage/emulated/0/Developer/result2.mp4").exists()) {
+		 * Log.d("ffmpeg","Success file:"+
+		 * "/storage/emulated/0/Developer/result2.mp4");
+		 * Toast.makeText(MainActivity.this, "result: WIN",
+		 * Toast.LENGTH_LONG).show(); } } } }); //fc.getInfo(clip_in); } catch
+		 * (FileNotFoundException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } catch (IOException e) { // TODO Auto-generated
+		 * catch block e.printStackTrace(); } catch (InterruptedException e) {
+		 * // TODO Auto-generated catch block e.printStackTrace(); } catch
+		 * (Exception e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
+		setContentView(R.layout.activity_main);
 
-			    @Override
-			    public void shellOut(String shellLine) {
-			        System.out.println("MIX> " + shellLine);
-			    }
+		// Initializing
+		dataList = new ArrayList<DrawerItem>();
+		mTitle = mDrawerTitle = getTitle();
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-			    @Override
-			    public void processComplete(int exitValue) {
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
 
-			        if (exitValue != 0) {
-			            System.err.println("concat non-zero exit: " + exitValue);
-			            Log.d("ffmpeg","Compilation error. FFmpeg failed");
-			            Toast.makeText(MainActivity.this, "result: ffmpeg failed", Toast.LENGTH_LONG).show();
-			        } else {
-			            if(new File( "/storage/emulated/0/Developer/result2.mp4").exists()) {
-			                Log.d("ffmpeg","Success file:"+ "/storage/emulated/0/Developer/result2.mp4");
-			                Toast.makeText(MainActivity.this, "result: WIN", Toast.LENGTH_LONG).show();
-			            }
-			        }
-			    }
-			});
-			//fc.getInfo(clip_in);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// Add Drawer Item to dataList
+		dataList.add(new DrawerItem("Convert Videos"));
+		dataList.add(new DrawerItem("Browse",
+				R.drawable.ic_action_import_export));
+		dataList.add(new DrawerItem("Output", R.drawable.ic_action_video));
+		dataList.add(new DrawerItem("Processing", R.drawable.ic_action_good));
+		dataList.add(new DrawerItem("Other Options"));
+		dataList.add(new DrawerItem("About", R.drawable.ic_action_about));
+
+		adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item,
+				dataList);
+
+		mDrawerList.setAdapter(adapter);
+
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		if (savedInstanceState == null) {
+			if (dataList.get(0).getTitle() != null) {
+				SelectItem(1);
+			} else {
+				SelectItem(0);
+			}
 		}
-	    */
-        setContentView(R.layout.activity_main);
-        
-        // Initializing
-        dataList = new ArrayList<DrawerItem>();
-        mTitle = mDrawerTitle = getTitle();
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+	}
 
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
-                    GravityCompat.START);
-         
-        // Add Drawer Item to dataList
-        dataList.add(new DrawerItem("Convert Videos"));
-        dataList.add(new DrawerItem("Browse", R.drawable.ic_action_import_export));
-        dataList.add(new DrawerItem("Output", R.drawable.ic_action_video));
-        dataList.add(new DrawerItem("Processing", R.drawable.ic_action_good));
-        dataList.add(new DrawerItem("Other Options"));
-        dataList.add(new DrawerItem("About", R.drawable.ic_action_about));
-        
-        adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item,
-                dataList);
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.main, menu);
+	// return true;
+	// }
 
-        mDrawerList.setAdapter(adapter);
-        
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener()); 
-        
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-         
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                    R.drawable.ic_drawer, R.string.drawer_open,
-                    R.string.drawer_close) {
-              public void onDrawerClosed(View view) {
-                    getActionBar().setTitle(mTitle);
-                    invalidateOptionsMenu(); // creates call to
-                                                              // onPrepareOptionsMenu()
-              }
-         
-              public void onDrawerOpened(View drawerView) {
-                    getActionBar().setTitle(mDrawerTitle);
-                    invalidateOptionsMenu(); // creates call to
-                                                              // onPrepareOptionsMenu()
-              }
-        };
-         
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-         
-        if (savedInstanceState == null) {
-	        if (dataList.get(0).getTitle() != null) {
-	            SelectItem(1);
-	        } else {
-	            SelectItem(0);
-	        }
-        }
-    }
-    
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//          // Inflate the menu; this adds items to the action bar if it is present.
-//          getMenuInflater().inflate(R.menu.main, menu);
-//          return true;
-//    }
-    
-    public void SelectItem(int possition) {
-    	 
-        Fragment fragment = null;
-        Bundle args = new Bundle();
-        switch (possition) {
-        case 1:
-              fragment = new FragmentBrowser();
-              args.putString(FragmentBrowser.ITEM_NAME, dataList.get(possition)
-                          .getItemName());
-              args.putInt(FragmentBrowser.IMAGE_RESOURCE_ID, dataList.get(possition)
-                          .getImgResID());
-              break;
-        case 2:
-              fragment = new FragmentOutput();
-              args.putString(FragmentOutput.ITEM_NAME, dataList.get(possition)
-                          .getItemName());
-              args.putInt(FragmentOutput.IMAGE_RESOURCE_ID, dataList.get(possition)
-                          .getImgResID());
-              break;
-        case 3:
-              fragment = new FragmentProcessing();
-              args.putString(FragmentProcessing.ITEM_NAME, dataList.get(possition)
-                          .getItemName());
-              args.putInt(FragmentProcessing.IMAGE_RESOURCE_ID, dataList.get(possition)
-                          .getImgResID());
-              break;        
-        case 5:
-              fragment = new FragmentOutput();
-              args.putString(FragmentOutput.ITEM_NAME, dataList.get(possition)
-                          .getItemName());
-              args.putInt(FragmentOutput.IMAGE_RESOURCE_ID, dataList.get(possition)
-                          .getImgResID());
-              break;
-        
-        default:
-              break;
-        }
+	public void SelectItem(int possition) {
+		Fragment fragment = null;
+		Bundle args = new Bundle();
+		switch (possition) {
+		case 1:
+			fragment = new FragmentBrowser();
+			break;
+		case 2:
+			fragment = new FragmentOutput();
+			args.putString(FragmentOutput.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentOutput.IMAGE_RESOURCE_ID, dataList.get(possition)
+                    .getImgResID());
+			break;
+		case 3:
+			fragment = new FragmentProcessing();
+			args.putString(FragmentProcessing.ITEM_NAME, dataList
+					.get(possition).getItemName());
+			args.putInt(FragmentProcessing.IMAGE_RESOURCE_ID,
+					dataList.get(possition).getImgResID());
+			break;
+		case 5:
+			fragment = new FragmentOutput();
+			args.putString(FragmentOutput.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentOutput.IMAGE_RESOURCE_ID,
+					dataList.get(possition).getImgResID());
+			break;
 
-        fragment.setArguments(args);
-        FragmentManager frgManager = getFragmentManager();
-        frgManager.beginTransaction().replace(R.id.content_frame, fragment)
-                    .commit();
+		default:
+			break;
+		}
 
-        mDrawerList.setItemChecked(possition, true);
-        setTitle(dataList.get(possition).getItemName());
-        mDrawerLayout.closeDrawer(mDrawerList);
+		fragment.setArguments(args);
+		FragmentManager frgManager = getFragmentManager();
+		frgManager.beginTransaction().replace(R.id.content_frame, fragment)
+				.commit();
 
-  }
-    
-    @Override
-    public void setTitle(CharSequence title) {
-          mTitle = title;
-          getActionBar().setTitle(mTitle);
-    }
-     
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-          super.onPostCreate(savedInstanceState);
-          // Sync the toggle state after onRestoreInstanceState has occurred.
-          mDrawerToggle.syncState();
-    }
-     
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-          // The action bar home/up action should open or close the drawer.
-          // ActionBarDrawerToggle will take care of this.
-          if (mDrawerToggle.onOptionsItemSelected(item)) {
-                return true;
-          }
-     
-          return false;
-    }
-     
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-          super.onConfigurationChanged(newConfig);
-          // Pass any configuration change to the drawer toggles
-          mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-    
-    private class DrawerItemClickListener implements
-    ListView.OnItemClickListener {
+		mDrawerList.setItemChecked(possition, true);
+		setTitle(dataList.get(possition).getItemName());
+		mDrawerLayout.closeDrawer(mDrawerList);
+
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// The action bar home/up action should open or close the drawer.
+		// ActionBarDrawerToggle will take care of this.
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggles
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
-		          long id) {
-		    if (dataList.get(position).getTitle() == null) {
-		          SelectItem(position);
-		    }
-		
+				long id) {
+			if (dataList.get(position).getTitle() == null) {
+				SelectItem(position);
+			}
+
 		}
-    }
+	}
 }
