afeb488234813a7bb5c4e1f9ac619a288e2c3b7b
Alain Pimentel <hpimentel@southern.edu>
2014-05-23 07:28:26 -0700

    Added aFileChooser lib for browsing files. Needs fixes: put activity in drawer

--- src/com/example/ffmpeg_trial/FragmentBrowser.java
+++ src/com/example/ffmpeg_trial/FragmentBrowser.java
@@ -1,39 +1,35 @@
 package com.example.ffmpeg_trial;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
- 
+
+
+
 public class FragmentBrowser extends Fragment {
- 
-      ImageView ivIcon;
-      TextView tvItemName;
- 
-      public static final String IMAGE_RESOURCE_ID = "iconResourceID";
-      public static final String ITEM_NAME = "itemName";
- 
-      public FragmentBrowser() {
- 
-      }
- 
-      @Override
-      public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                  Bundle savedInstanceState) {
- 
-            View view = inflater.inflate(R.layout.fragment_layout_browser, container,
-                        false);
- 
-            ivIcon = (ImageView) view.findViewById(R.id.frag1_icon);
-            tvItemName = (TextView) view.findViewById(R.id.frag1_text);
- 
-            tvItemName.setText(getArguments().getString(ITEM_NAME));
-            ivIcon.setImageDrawable(view.getResources().getDrawable(
-                        getArguments().getInt(IMAGE_RESOURCE_ID)));
-            return view;
-      }
- 
+
+	ImageView ivIcon;
+	TextView tvItemName;
+
+	public static final String IMAGE_RESOURCE_ID = "iconResourceID";
+	public static final String ITEM_NAME = "itemName";
+
+	public FragmentBrowser() {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_layout_browser,container,false);
+		
+		Intent i = new Intent(getActivity(), ActivityaFileChooserLink.class);
+		startActivity(i);
+	    return view;
+	}
 }
